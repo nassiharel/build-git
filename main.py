f0df32538dee3524c003c1b28fdc142f5a274999
Nassi Harel <nassi9@gmail.com>
2020-02-09 16:40:52 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+def start(args):
+    print('algorithm: start')
+  
+    input = args['input']
+    array = input[0]
+    order = input[1]
+    if order == 'asc':
+        reverse = False
+    elif order == 'desc':
+        reverse = True
+    else:
+        raise Exception('order not supported')
+
+    list.sort(array, reverse=reverse)
+    return array
+
+
